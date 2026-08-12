a647346de46352682a48beaa10d8a030384b6f77
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-12 13:25:28 +0300

    Create PaginationExtensions

---
+++ BasiQBLL/Extensions/PaginationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using BasicBLL.DTOs.PaginationDTOs;
+
+namespace BasicBLL.Extensions
+{
+    public static class PaginationExtensions
+    {
+        public static PagedResultDTO<TDestination> ToPagedResult<TSource, TDestination>(
+        this (IEnumerable<TSource> Data, int TotalCount) result,
+        IEnumerable<TDestination> mappedItems,
+        PaginationParametersDTO pagination)
+        {
+            return new PagedResultDTO<TDestination>
+            {
+                Items = mappedItems,
+                TotalCount = result.TotalCount,
+                PageNumber = pagination.PageNumber,
+                PageSize = pagination.PageSize
+            };
+        }
+    }
+}
