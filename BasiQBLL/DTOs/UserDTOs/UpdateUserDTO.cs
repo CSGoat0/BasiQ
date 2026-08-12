7a8fa8da565e9c69b59b05cdc22cd504b2ebbd48
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-12 13:22:55 +0300

    Create UpdateUserDTO

---
+++ BasiQBLL/DTOs/UserDTOs/UpdateUserDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace BasicBLL.DTOs.UserDTOs
+{
+    public class UpdateUserDTO
+    {
+        public string? Id { get; set; }
+        public string? FullName { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? ImgPath { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+    }
+}
