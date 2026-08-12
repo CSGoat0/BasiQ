86d51142baa0bf1335f8e0f09d2d859d041412bc
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-12 13:20:18 +0300

    Create ChangePasswordDTO

---
+++ BasiQBLL/UserDTOs/ChangePasswordDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicBLL.DTOs.UserDTOs
+{
+    public class ChangePasswordDTO
+    {
+        public string? CurrentPassword { get; set; }
+        public string? NewPassword { get; set; }
+        public string? ConfirmPassword { get; set; }
+    }
+}
