674ceba4fc94c9bca27a328ab42435e59ccb2b75
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-12 13:19:24 +0300

    Create ServiceResponse<T>

---
+++ BasiQBLL/DTOs/ServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicBLL.DTOs
+{
+    public class ServiceResponse<T>
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+    }
+}
