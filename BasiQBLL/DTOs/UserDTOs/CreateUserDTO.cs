576baddf5970d95fef3634db11864a3c5087e2c2
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-12 13:22:04 +0300

    Create CreateUserDTO

---
+++ BasiQBLL/DTOs/UserDTOs/CreateUserDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace BasicBLL.DTOs.UserDTOs
+{
+    public class CreateUserDTO
+    {
+        public string? UserName { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Password { get; set; }
+        public string? ImgPath { get; set; }
+        public string? Address { get; set; }
+    }
+}
