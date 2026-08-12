1376b64dd9e6737b9d5c3e19eeef9745d05750f3
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-12 13:24:12 +0300

    Create PaginationParametersDTO

---
+++ BasiQBLL/DTOs/PaginationDTOs/PaginationParametersDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace BasicBLL.DTOs.PaginationDTOs
+{
+    public class PaginationParametersDTO
+    {
+
+        private const int MaxPageSize = 50;
+
+        private int _pageSize = 10;
+        /// <example>1</example>
+        public int PageNumber { get; set; } = 1;
+        /// <example>10</example>
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+        }
+    }
+}
