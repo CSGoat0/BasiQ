d1e52721987c3a365b78c98f190edd5db6331c63
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-12 13:22:40 +0300

    Create ResetPasswordResponseDTO

---
+++ BasiQBLL/DTOs/UserDTOs/ResetPasswordResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicBLL.DTOs.UserDTOs
+{
+    public class ResetPasswordResponseDTO
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Token { get; set; }
+    }
+}
