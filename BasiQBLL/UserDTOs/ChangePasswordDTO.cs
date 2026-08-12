2674cf82b99af6c9709fe10241f41694f2c57906
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-12 13:21:14 +0300

    Remove wrong file

--- BasiQBLL/UserDTOs/ChangePasswordDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BasicBLL.DTOs.UserDTOs
-{
-    public class ChangePasswordDTO
-    {
-        public string? CurrentPassword { get; set; }
-        public string? NewPassword { get; set; }
-        public string? ConfirmPassword { get; set; }
-    }
-}
