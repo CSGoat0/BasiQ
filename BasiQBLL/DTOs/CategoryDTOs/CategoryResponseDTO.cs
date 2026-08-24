9bd9b48c11938c9ec39ad407df0ae327c5c2fbff
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-24 18:34:30 +0300

    Create CategoryResponseDTO

---
+++ BasiQBLL/DTOs/CategoryDTOs/CategoryResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace BasiQBLL.DTOs.CategoryDTOs
+{
+    public class CategoryResponseDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int ProductCount { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? RegistrationDate { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public DateTime? DeletedOn { get; set; }
+    }
+}
