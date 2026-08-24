472a37bd5238f922af868a059677813169f07d34
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-24 18:24:19 +0300

    Create UpdateMarketStatusDTO

---
+++ BasiQBLL/DTOs/MarketDTOs/UpdateMarketStatusDTO.cs
@@ -0,0 +1,14 @@
+﻿using BasiQDAL.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasiQBLL.DTOs.MarketDTOs
+{
+    public class UpdateMarketStatusDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public MarketStatus Status { get; set; }
+    }
+}
