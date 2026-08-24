664b88da0bf53683f37f104635da22c786a3c9a2
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-24 19:01:30 +0300

    Create ReorderProductImagesDTO

---
+++ BasiQBLL/DTOs/ProductImageDTOs/ReorderProductImagesDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasiQBLL.DTOs.ProductImageDTOs
+{
+    public class ReorderProductImagesDTO
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public List<int> ImageIdsInOrder { get; set; } = new List<int>();
+    }
+}
