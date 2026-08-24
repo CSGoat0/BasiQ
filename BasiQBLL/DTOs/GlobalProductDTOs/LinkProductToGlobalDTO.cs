332314b9feeebdec521e6989483338b76e931e98
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-24 18:37:28 +0300

    Create LinkProductToGlobalDTO

---
+++ BasiQBLL/DTOs/GlobalProductDTOs/LinkProductToGlobalDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasiQBLL.DTOs.GlobalProductDTOs
+{
+    public class LinkProductToGlobalDTO
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int GlobalProductId { get; set; }
+    }
+}
