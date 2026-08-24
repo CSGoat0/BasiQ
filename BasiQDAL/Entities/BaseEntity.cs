dfd9e76bac0cca51e2c860ac4b6e3ca37ba612e7
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-24 17:38:30 +0300

    Create BaseEntity to be inherited by most entities

---
+++ BasiQDAL/Entities/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿namespace BasiQDAL.Entities
+{
+    public abstract class BaseEntity
+    {
+        public bool IsDeleted { get; private set; }
+        public DateTime? DeletedOn { get; private set; }
+        public DateTime? UpdatedOn { get; private set; }
+        public DateTime? RegistrationDate { get; private set; } = DateTime.Now;
+
+        public virtual void Delete()
+        {
+            IsDeleted = true;
+            DeletedOn = DateTime.Now;
+        }
+
+        public virtual void Restore()
+        {
+            IsDeleted = false;
+            UpdatedOn = DateTime.Now;
+        }
+
+        protected void UpdateTimestamp()
+        {
+            UpdatedOn = DateTime.Now;
+        }
+    }
+}
