2d817b2d956cc712c79b957a98b4219053345955
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-24 17:44:35 +0300

    Create Category class

---
+++ BasiQDAL/Entities/Category.cs
@@ -0,0 +1,40 @@
+﻿namespace BasiQDAL.Entities
+{
+    public class Category : BaseEntity
+    {
+        public int Id { get; private set; }
+        public string? Name { get; private set; }
+        public string? Description { get; private set; }
+
+        // Navigation Properties
+        public virtual ICollection<Product>? Products { get; private set; }
+
+        protected Category() { }
+
+        public Category(string? name, string? description = null)
+        {
+            Name = name;
+            Description = description;
+            Products = new List<Product>();
+        }
+
+        // ===== Edit Methods =====
+        public void EditName(string? name)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                Name = name;
+                UpdateTimestamp();
+            }
+        }
+
+        public void EditDescription(string? description)
+        {
+            if (!string.IsNullOrWhiteSpace(description))
+            {
+                Description = description;
+                UpdateTimestamp();
+            }
+        }
+    }
+}
