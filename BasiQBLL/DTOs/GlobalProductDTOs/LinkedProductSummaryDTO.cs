033e0b1ac9e32a32e482fb262db9d4f3a344d133
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-24 18:36:53 +0300

    Create LinkedProductSummaryDTO

---
+++ BasiQBLL/DTOs/GlobalProductDTOs/LinkedProductSummaryDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace BasiQBLL.DTOs.GlobalProductDTOs
+{
+    public class LinkedProductSummaryDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int MarketId { get; set; }
+        public string? MarketName { get; set; }
+        public double? CurrentPrice { get; set; }
+        public bool IsInStock { get; set; }
+        public string? PrimaryImageUrl { get; set; }
+    }
+}
