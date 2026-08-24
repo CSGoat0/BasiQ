a2466e2aae915f6be86404fbd3aa0eb669b9a8ae
CSGoat0 <elturkabdelrahman@gmail.com>
2026-08-24 19:02:20 +0300

    Create RejectProductDTO

---
+++ BasiQBLL/DTOs/RejectionDTOs/RejectProductDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasiQBLL.DTOs.RejectionDTOs
+{
+    public class RejectProductDTO
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Rejection reason is required.")]
+        [StringLength(1000, MinimumLength = 5, ErrorMessage = "Rejection reason must be between 5 and 1000 characters.")]
+        public string? Reason { get; set; }
+
+        public string? RejectedByUserId { get; set; }
+    }
+}
